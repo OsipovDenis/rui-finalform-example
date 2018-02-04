036d306e777dc3d2136e9656a6d4362579a5e325
Denis Osipov <d.osipov@rambler-co.ru>
2018-02-04 21:49:25 +0300

    simplest webpack config

---
+++ README.md
@@ -0,0 +1,8 @@
+My try to understand how to work rambler-ui with final-form
+
+```
+npm install
+npm run dev
+```
+
+go to http://localhost:9000/
---
+++ webpack.config.babel.js
@@ -0,0 +1,34 @@
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'App',
+      filename: './src/index.html',
+      inject: 'body'
+    })
+  ]
+}
+
+export default config
